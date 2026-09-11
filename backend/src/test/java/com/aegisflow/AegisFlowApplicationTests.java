b5db7396f44a3689bc099b975e9210cdc7909d5e
Naman Jadiya <namanjadiya2006@gmail.com>
2026-09-11 13:14:59 +0530

    folder structure corrected (#3)

---
+++ backend/src/test/java/com/aegisflow/AegisFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aegisflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AegisFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
